70baaa75972a0addc52b3160258dae48414f71c5
matteocicigoi <matteo.cicigoi1@hotmail.it>
2023-11-14 12:03:54 +0100

    feat: snack5

---
+++ snack-5/js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+Fai inserire un numero, che chiameremo N, all’utente.
+Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100. Ogni volta che ne crei uno, stampalo a schermo.
+*/
+const numero = +prompt('inserisci un numero');
+for(let i = 0; i < numero; i++){
+    console.log(i);
+    const numeri = [];
+    for(let y = 0; y < 10; y++){
+        numeri.push(Math.floor(Math.random() * 100 + 1));
+    }
+    alert('Array: ' + i + ' - ' + numeri);
+}
